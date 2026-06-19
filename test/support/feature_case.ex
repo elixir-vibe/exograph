b2eb2e578c9739fc273f280a97850b92a234f945
Danila Poyarkov <dev@dannote.net>
2026-06-19 17:07:36 +0200

    Start feature services from feature cases

--- test/support/feature_case.ex
+++ test/support/feature_case.ex
@@ -10,6 +10,14 @@ defmodule Exograph.FeatureCase do
   end
 
   setup_all _context do
+    if Code.ensure_loaded?(PhoenixTest.Playwright.Supervisor) do
+      case PhoenixTest.Playwright.Supervisor.start_link() do
+        {:ok, _pid} -> :ok
+        {:error, {:already_started, _pid}} -> :ok
+      end
+    end
+
+    Exograph.Test.WebSetup.ensure_started!()
     Application.put_env(:phoenix_test, :base_url, Exograph.Test.WebSetup.base_url())
     :ok
   end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,13 +1,1 @@
 ExUnit.start(exclude: [:feature])
-
-feature_tests? =
-  System.get_env("EXOGRAPH_FEATURE_TESTS") ||
-    Enum.any?(System.argv(), &String.contains?(&1, "feature"))
-
-if feature_tests? and Code.ensure_loaded?(PhoenixTest.Playwright.Supervisor) do
-  {:ok, _} = PhoenixTest.Playwright.Supervisor.start_link()
-end
-
-if feature_tests? do
-  Exograph.Test.WebSetup.ensure_started!()
-end
