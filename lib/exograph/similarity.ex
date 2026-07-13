335c52267e9c647c76b74667759d947c34d1f9b9
Danila Poyarkov <dev@dannote.net>
2026-07-13 12:12:00 +0200

    Prefilter similarity candidates by subhash

--- lib/exograph/similarity.ex
+++ lib/exograph/similarity.ex
@@ -1,9 +1,12 @@
 defmodule Exograph.Similarity do
   @moduledoc false
 
+  import Ecto.Query
+  import QuackDB.Ecto.List, only: [has_any: 2]
+
   alias ExDNA.AST.{EditDistance, Fingerprint, Normalizer}
-  alias Exograph.Storage.FragmentStore
   alias Exograph.Index
+  alias Exograph.Storage.{FragmentRecord, FragmentStore, Hydration, Schema}
 
   @default_opts [min_mass: 8, min_similarity: 0.8, limit: 20]
 
@@ -12,12 +15,11 @@ defmodule Exograph.Similarity do
     opts = Keyword.merge(@default_opts, opts)
 
     with {:ok, query_fragment} <- query_fragment(source_or_ast, opts) do
-      fragments = FragmentStore.all(index.fragment_store)
       query_norm = Normalizer.normalize(query_fragment.ast)
 
       results =
-        fragments
-        |> Enum.filter(&mass_compatible?(&1, query_fragment))
+        index
+        |> candidate_fragments(query_fragment)
         |> Enum.map(fn fragment ->
           overlap = subhash_overlap(fragment, query_fragment)
           similarity = EditDistance.similarity(query_norm, Normalizer.normalize(fragment.ast))
@@ -34,6 +36,32 @@ defmodule Exograph.Similarity do
         |> Enum.take(opts[:limit])
 
       {:ok, results}
+    end
+  end
+
+  defp candidate_fragments(index, query_fragment) do
+    hashes = MapSet.to_list(query_fragment.sub_hashes)
+    source = Schema.fragments_source(index.fragment_store.prefix)
+    files_source = Schema.files_source(index.fragment_store.prefix)
+
+    candidates =
+      from(fragment in {source, FragmentRecord},
+        join: file in ^files_source,
+        on: file.id == fragment.file_id,
+        where: fragment.mass >= ^div(query_fragment.mass, 2),
+        where: fragment.mass <= ^(query_fragment.mass * 2),
+        where: has_any(fragment.sub_hashes, ^hashes),
+        order_by: [asc: file.path, asc: fragment.line, asc: fragment.id],
+        select: {fragment, file.source, file.path, file.ast}
+      )
+      |> index.fragment_store.repo.all()
+      |> Enum.map(fn {record, source, path, file_ast} ->
+        Hydration.fragment(record, source, path, nil, nil, file_ast)
+      end)
+
+    case candidates do
+      [] -> FragmentStore.all(index.fragment_store)
+      _ -> candidates
     end
   end
 
@@ -54,10 +82,6 @@ defmodule Exograph.Similarity do
       [fragment | _] -> {:ok, fragment}
       [] -> {:error, :query_too_small}
     end
-  end
-
-  defp mass_compatible?(fragment, query_fragment) do
-    min(fragment.mass, query_fragment.mass) / max(fragment.mass, query_fragment.mass) >= 0.5
   end
 
   defp subhash_overlap(fragment, query_fragment) do
---
+++ test/exograph/similarity_test.exs
@@ -0,0 +1,40 @@
+defmodule Exograph.SimilarityTest do
+  use ExUnit.Case, async: false
+
+  alias Exograph.DuckDBSupport
+
+  test "uses indexed subhashes to find similar fragments" do
+    DuckDBSupport.start_managed_repo!()
+    prefix = "similarity_#{System.unique_integer([:positive])}"
+
+    {:ok, index} =
+      Exograph.index_sources(
+        [
+          {"lib/demo.ex",
+           """
+           defmodule Demo do
+             def target(value) do
+               Enum.map([value], fn item -> item + 1 end)
+             end
+           end
+           """}
+        ],
+        DuckDBSupport.opts(prefix, extractors: [:ex_ast], min_mass: 1)
+      )
+
+    assert {:ok, [%{fragment: fragment, similarity: similarity} | _]} =
+             Exograph.similar(
+               index,
+               """
+               def target(value) do
+                 Enum.map([value], fn item -> item + 1 end)
+               end
+               """,
+               min_mass: 1,
+               min_similarity: 0.5
+             )
+
+    assert fragment.name == "target"
+    assert similarity >= 0.5
+  end
+end
